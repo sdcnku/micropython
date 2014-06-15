1e50322a9757e6583ea448e759f056eda66c530c
mux <freelancer.c@gmail.com>
2014-06-15 21:14:23 +0200

    Fix pin definitions

--- stmhal/boards/OPENMV1/mpconfigboard.h
+++ stmhal/boards/OPENMV1/mpconfigboard.h
@@ -26,8 +26,7 @@
 // LEDs
 #define MICROPY_HW_LED1             (pin_D4) // red
 #define MICROPY_HW_LED2             (pin_D6) // green
-#define MICROPY_HW_LED3             (pin_D6) // orange
-#define MICROPY_HW_LED4             (pin_D5) // blue
+#define MICROPY_HW_LED3             (pin_D5) // blue
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
-#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
-#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRL = pin->pin_mask)
