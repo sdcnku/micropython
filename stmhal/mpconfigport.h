b888b31f32df736eb12d3439b0b1f7d966ed9cf4
iabdalkader <i.abdalkader@gmail.com>
2015-08-09 12:59:09 +0200

    Export image module

--- stmhal/mpconfigport.h
+++ stmhal/mpconfigport.h
@@ -109,6 +109,7 @@ extern const struct _mp_obj_module_t mp_module_network;
 extern const struct _mp_obj_module_t time_module;
 extern const struct _mp_obj_module_t mlx_module;
 extern const struct _mp_obj_module_t sensor_module;
+extern const struct _mp_obj_module_t image_module;
 
 
 #define MICROPY_PORT_BUILTIN_MODULES \
@@ -121,7 +122,8 @@ extern const struct _mp_obj_module_t sensor_module;
     { MP_OBJ_NEW_QSTR(MP_QSTR_network), (mp_obj_t)&mp_module_network }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_time),    (mp_obj_t)&time_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_mlx),     (mp_obj_t)&mlx_module }, \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_sensor),  (mp_obj_t)&sensor_module },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_sensor),  (mp_obj_t)&sensor_module }, \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_image),   (mp_obj_t)&image_module },
 
 
 #define MICROPY_PORT_BUILTIN_MODULE_WEAK_LINKS \
