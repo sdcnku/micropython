a9ff8b6f93165b7885885192159ea42ac6adb9af
iabdalkader <i.abdalkader@gmail.com>
2016-08-13 22:05:41 +0200

    Resize USBD string descriptor buffer

--- stmhal/usbd_conf.h
+++ stmhal/usbd_conf.h
@@ -46,7 +46,9 @@
 /* Common Config */
 #define USBD_MAX_NUM_INTERFACES               3
 #define USBD_MAX_NUM_CONFIGURATION            1
-#define USBD_MAX_STR_DESC_SIZ                 0x100
+// Max len is (strlen(desc) * 2 + 2);
+// TODO hard-code descriptors and remove this buffer
+#define USBD_MAX_STR_DESC_SIZ                 100
 #define USBD_SUPPORT_USER_STRING              0
 #define USBD_SELF_POWERED                     0
 #define USBD_DEBUG_LEVEL                      0
