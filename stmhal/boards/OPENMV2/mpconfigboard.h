18cd4fc5b10941c57637bbcee7681f1b891ec889
iabdalkader <i.abdalkader@gmail.com>
2017-10-01 22:57:06 +0200

    Fix GPIO for new M4 HAL.

--- stmhal/boards/OPENMV2/mpconfigboard.h
+++ stmhal/boards/OPENMV2/mpconfigboard.h
@@ -61,5 +61,5 @@
 #define MICROPY_HW_LED3             (pin_C1) // blue
 #define MICROPY_HW_LED4             (pin_E2) // IR 
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
-#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRH = pin->pin_mask)
-#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRL = pin->pin_mask)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRR = (pin->pin_mask << 16))
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRR = pin->pin_mask)
