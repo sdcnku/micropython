ede6e0c04c5ea7550b34dcdbcd959c72881f3c88
iabdalkader <i.abdalkader@gmail.com>
2019-06-20 21:59:30 +0200

    Add OPENMV4R board files.

---
+++ ports/stm32/boards/OPENMV4R/mpconfigboard.h
@@ -0,0 +1,132 @@
+#define MICROPY_HW_BOARD_NAME       "OPENMV4R"
+#define MICROPY_HW_MCU_NAME         "STM32H743"
+#define MICROPY_PY_SYS_PLATFORM     "OpenMV4R-H7"
+
+#define MICROPY_HW_HAS_SDCARD       (1)
+#define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (1)
+#define MICROPY_HW_ENABLE_ADC       (1)
+#define MICROPY_HW_ENABLE_SPI2      (1)
+#define MICROPY_HW_ENABLE_USB       (1)
+#define MICROPY_FATFS_EXFAT         (1)
+
+// Note these are not used in top system.c.
+#define MICROPY_HW_CLK_PLLM         (3)
+#define MICROPY_HW_CLK_PLLN         (200)
+#define MICROPY_HW_CLK_PLLP         (2)
+#define MICROPY_HW_CLK_PLLQ         (8)
+#define MICROPY_HW_CLK_PLLR         (2)
+
+// UART1 config
+#define MICROPY_HW_UART1_TX  (pin_B14)
+#define MICROPY_HW_UART1_RX  (pin_B15)
+
+// UART3 config
+#define MICROPY_HW_UART3_TX  (pin_B10)
+#define MICROPY_HW_UART3_RX  (pin_B11)
+#define MICROPY_HW_UART3_RTS (pin_B14)
+#define MICROPY_HW_UART3_CTS (pin_B13)
+
+// I2C buses
+#define MICROPY_HW_I2C2_SCL (pin_B10)
+#define MICROPY_HW_I2C2_SDA (pin_B11)
+
+#define MICROPY_HW_I2C4_SCL (pin_D12)
+#define MICROPY_HW_I2C4_SDA (pin_D13)
+
+// SPI buses
+#define MICROPY_HW_SPI2_NSS  (pin_B12)
+#define MICROPY_HW_SPI2_SCK  (pin_B13)
+#define MICROPY_HW_SPI2_MISO (pin_B14)
+#define MICROPY_HW_SPI2_MOSI (pin_B15)
+
+// CAN busses
+//#define MICROPY_HW_CAN2_NAME "CAN2" // CAN2 on RX,TX = P3,P2 = PB12,PB13
+//#define MICROPY_HW_CAN2_TX          (pin_B13)
+//#define MICROPY_HW_CAN2_RX          (pin_B12)
+
+// SD card detect switch
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_D0)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+
+// USB config
+#define MICROPY_HW_USB_FS                   (1)
+#define MICROPY_HW_USB_VBUS_DETECT_PIN      (pin_A9)
+
+// LEDs
+#define MICROPY_HW_LED1             (pin_C0) // red
+#define MICROPY_HW_LED2             (pin_C1) // green
+#define MICROPY_HW_LED3             (pin_C2) // blue
+#define MICROPY_HW_LED4             (pin_E2) // IR
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRH = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRL = pin->pin_mask)
+
+// SDRAM
+#define MICROPY_HW_SDRAM_SIZE  (256 / 16 * 1024 * 1024)  // 16 M byte
+#define MICROPY_HW_SDRAM_STARTUP_TEST             (1)
+
+// Timing configuration for 143 Mhz (7ns)
+#define MICROPY_HW_SDRAM_TIMING_TMRD        (2)
+#define MICROPY_HW_SDRAM_TIMING_TXSR        (7)
+#define MICROPY_HW_SDRAM_TIMING_TRAS        (4)
+#define MICROPY_HW_SDRAM_TIMING_TRC         (7)
+#define MICROPY_HW_SDRAM_TIMING_TWR         (2)
+#define MICROPY_HW_SDRAM_TIMING_TRP         (2)
+#define MICROPY_HW_SDRAM_TIMING_TRCD        (2)
+#define MICROPY_HW_SDRAM_REFRESH_RATE       (64) // ms
+
+#define MICROPY_HW_SDRAM_BURST_LENGTH       1
+#define MICROPY_HW_SDRAM_CAS_LATENCY        2
+#define MICROPY_HW_SDRAM_COLUMN_BITS_NUM    8
+#define MICROPY_HW_SDRAM_ROW_BITS_NUM       12
+#define MICROPY_HW_SDRAM_MEM_BUS_WIDTH      16
+#define MICROPY_HW_SDRAM_INTERN_BANKS_NUM   4
+#define MICROPY_HW_SDRAM_CLOCK_PERIOD       2
+#define MICROPY_HW_SDRAM_RPIPE_DELAY        0
+#define MICROPY_HW_SDRAM_RBURST             (1)
+#define MICROPY_HW_SDRAM_WRITE_PROTECTION   (0)
+#define MICROPY_HW_SDRAM_AUTOREFRESH_NUM    (8)
+
+#define MICROPY_HW_FMC_SDCKE0   (pin_C5)
+#define MICROPY_HW_FMC_SDNE0    (pin_C4)
+#define MICROPY_HW_FMC_SDCLK    (pin_G8)
+#define MICROPY_HW_FMC_SDNCAS   (pin_G15)
+#define MICROPY_HW_FMC_SDNRAS   (pin_F11)
+#define MICROPY_HW_FMC_SDNWE    (pin_A7)
+#define MICROPY_HW_FMC_BA0      (pin_G4)
+#define MICROPY_HW_FMC_BA1      (pin_G5)
+#define MICROPY_HW_FMC_NBL0     (pin_E0)
+#define MICROPY_HW_FMC_NBL1     (pin_E1)
+#define MICROPY_HW_FMC_A0       (pin_F0)
+#define MICROPY_HW_FMC_A1       (pin_F1)
+#define MICROPY_HW_FMC_A2       (pin_F2)
+#define MICROPY_HW_FMC_A3       (pin_F3)
+#define MICROPY_HW_FMC_A4       (pin_F4)
+#define MICROPY_HW_FMC_A5       (pin_F5)
+#define MICROPY_HW_FMC_A6       (pin_F12)
+#define MICROPY_HW_FMC_A7       (pin_F13)
+#define MICROPY_HW_FMC_A8       (pin_F14)
+#define MICROPY_HW_FMC_A9       (pin_F15)
+#define MICROPY_HW_FMC_A10      (pin_G0)
+#define MICROPY_HW_FMC_A11      (pin_G1)
+#define MICROPY_HW_FMC_D0       (pin_D14)
+#define MICROPY_HW_FMC_D1       (pin_D15)
+#define MICROPY_HW_FMC_D2       (pin_D0)
+#define MICROPY_HW_FMC_D3       (pin_D1)
+#define MICROPY_HW_FMC_D4       (pin_E7)
+#define MICROPY_HW_FMC_D5       (pin_E8)
+#define MICROPY_HW_FMC_D6       (pin_E9)
+#define MICROPY_HW_FMC_D7       (pin_E10)
+#define MICROPY_HW_FMC_D8       (pin_E11)
+#define MICROPY_HW_FMC_D9       (pin_E12)
+#define MICROPY_HW_FMC_D10      (pin_E13)
+#define MICROPY_HW_FMC_D11      (pin_E14)
+#define MICROPY_HW_FMC_D12      (pin_E15)
+#define MICROPY_HW_FMC_D13      (pin_D8)
+#define MICROPY_HW_FMC_D14      (pin_D9)
+#define MICROPY_HW_FMC_D15      (pin_D10)
