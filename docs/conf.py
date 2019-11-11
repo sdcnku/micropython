0769475820fe57f3333d54974aefd84461ba4594
Kwabena W. Agyeman <kwagyeman@live.com>
2019-11-10 17:41:45 -0800

    Update top level indexes

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'MicroPython'
-copyright = '2014-2019, Damien P. George, Paul Sokolovsky, and contributors'
+copyright = '2014-2019, Damien P. George, Paul Sokolovsky, OpenMV LLC, and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -154,12 +154,12 @@ else:
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = '../../logo/trans-logo.png'
+html_logo = '../../openmv-media/logos/openmv-logo-white/web-logo-sticky.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'favicon.ico'
+html_favicon = '../../openmv-media/icons/openmv-icon/openmv.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -236,7 +236,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'MicroPython.tex', 'MicroPython Documentation',
-   'Damien P. George, Paul Sokolovsky, and contributors', 'manual'),
+   'Damien P. George, Paul Sokolovsky, OpenMV LLC, and contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -266,7 +266,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'micropython', 'MicroPython Documentation',
-     ['Damien P. George, Paul Sokolovsky, and contributors'], 1),
+     ['Damien P. George, Paul Sokolovsky, OpenMV LLC, and contributors'], 1),
 ]
 
 # If true, show URL addresses after external links.
@@ -280,7 +280,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   (master_doc, 'MicroPython', 'MicroPython Documentation',
-   'Damien P. George, Paul Sokolovsky, and contributors', 'MicroPython', 'One line description of project.',
+   'Damien P. George, Paul Sokolovsky, OpenMV LLC, and contributors', 'MicroPython', 'One line description of project.',
    'Miscellaneous'),
 ]
 
