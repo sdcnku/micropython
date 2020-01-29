tytgatlieven/micropython-esp32c6

spiflash: Support spi/qspi flash chips over 16MB. Over 16MB, 32-bit addressing is required rather than the standard 24-bit.

drivers/bus/qspi.h
@@ -52,6 +52,22 @@ typedef struct _mp_soft_qspi_obj_t {
     mp_hal_pin_obj_t io3;
 } mp_soft_qspi_obj_t;
 
+#define MP_SPI_ADDR_32B(addr) (addr & 0xF000)
+
+STATIC inline uint8_t mp_spi_set_addr_buff(uint8_t *buf, uint32_t addr) {
+    if(MP_SPI_ADDR_32B(addr)) {
+        buf[0] = addr >> 24;
+        buf[1] = addr >> 16;
+        buf[2] = addr >> 8;
+        buf[3] = addr;
+        return 4;
+    } else {
+        buf[0] = addr >> 16;
+        buf[1] = addr >> 8;
+        buf[2] = addr;
+        return 3;
+    }
+}
 extern const mp_qspi_proto_t mp_soft_qspi_proto;
 
 #endif // MICROPY_INCLUDED_DRIVERS_BUS_QSPI_H

sdcnku/micropython

drivers/bus/qspi.h
@@ -52,6 +52,22 @@ typedef struct _mp_soft_qspi_obj_t {
     mp_hal_pin_obj_t io3;
 } mp_soft_qspi_obj_t;
 
+#define MP_SPI_ADDR_32B(addr) (addr & 0xFF000000)
+
+STATIC inline uint8_t mp_spi_set_addr_buff(uint8_t *buf, uint32_t addr) {
+    if(MP_SPI_ADDR_32B(addr)) {
+        buf[0] = addr >> 24;
+        buf[1] = addr >> 16;
+        buf[2] = addr >> 8;
+        buf[3] = addr;
+        return 4;
+    } else {
+        buf[0] = addr >> 16;
+        buf[1] = addr >> 8;
+        buf[2] = addr;
+        return 3;
+    }
+}
 extern const mp_qspi_proto_t mp_soft_qspi_proto;
 
 #endif // MICROPY_INCLUDED_DRIVERS_BUS_QSPI_H