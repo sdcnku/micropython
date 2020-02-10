699bd58f24fbd8dfe4899848892b4a0cd0579a12
Kwabena W. Agyeman <kwagyeman@live.com>
2020-02-09 20:56:51 -0800

    Add IMU module

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -216,6 +216,7 @@ extern const struct _mp_obj_module_t mp_module_usocket;
 extern const struct _mp_obj_module_t mp_module_network;
 extern const struct _mp_obj_module_t mp_module_onewire;
 extern const struct _mp_obj_module_t omv_module;
+extern const struct _mp_obj_module_t imu_module;
 extern const struct _mp_obj_module_t time_module;
 extern const struct _mp_obj_module_t sensor_module;
 extern const struct _mp_obj_module_t image_module;
@@ -280,6 +281,7 @@ extern const struct _mp_obj_module_t ulab_user_cmodule;
     { MP_OBJ_NEW_QSTR(MP_QSTR_uos), (mp_obj_t)&mp_module_uos }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_utime), (mp_obj_t)&mp_module_utime }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_omv),     (mp_obj_t)&omv_module }, \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_imu),     (mp_obj_t)&imu_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_time),    (mp_obj_t)&time_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_sensor),  (mp_obj_t)&sensor_module }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_image),   (mp_obj_t)&image_module }, \
