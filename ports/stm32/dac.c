d3e08a96c893c9b5f876df474cf4de010d8f87be
iabdalkader <i.abdalkader@gmail.com>
2021-06-19 22:03:11 +0200

    stm32: fix dma_nohal_init for H7.

--- ports/stm32/dac.c
+++ ports/stm32/dac.c
@@ -162,27 +162,7 @@ STATIC void dac_start(uint32_t dac_channel) {
     DAC->CR |= DAC_CR_EN1 << dac_channel;
 }
 
-#if defined(STM32H7)
-// Workaround until dma_nohal functions get fixed.
-static DAC_HandleTypeDef hdac;
-static DMA_HandleTypeDef hdma;
-extern DMA_InitTypeDef dma_init_struct_dac;
-
-STATIC void dac_start_dma(uint32_t dac_channel, const dma_descr_t *dma_descr, uint32_t dma_mode, uint32_t bit_size, uint32_t dac_align, size_t len, void *buf) {
-    hdac.Instance = DAC1;
-    HAL_DAC_Init(&hdac);
-
-    dma_deinit(dma_descr);
-    // Currently there's no other way to set the mode when using this API.
-    dma_init_struct_dac.Mode = dma_mode;
-    dma_init(&hdma, dma_descr, DMA_MEMORY_TO_PERIPH, &hdac);
-    hdac.DMA_Handle2 = &hdma;
-
-    MP_HAL_CLEAN_DCACHE(buf, len);
-    HAL_DAC_Start_DMA(&hdac, dac_channel, (uint32_t *)buf, len, DAC_ALIGN_8B_R);
-}
-
-#elif !defined(STM32F4)
+#if !defined(STM32F4)
 STATIC void dac_start_dma(uint32_t dac_channel, const dma_descr_t *dma_descr, uint32_t dma_mode, uint32_t bit_size, uint32_t dac_align, size_t len, void *buf) {
     uint32_t dma_align;
     if (bit_size == 8) {
@@ -199,6 +179,10 @@ STATIC void dac_start_dma(uint32_t dac_channel, const dma_descr_t *dma_descr, ui
         base = (uint32_t)&DAC->DHR12R2;
     #endif
     }
+
+    #ifdef __DCACHE_PRESENT
+    MP_HAL_CLEAN_DCACHE(buf, len);
+    #endif
 
     dma_nohal_deinit(dma_descr);
     dma_nohal_init(dma_descr, DMA_MEMORY_TO_PERIPH | dma_mode | dma_align);
