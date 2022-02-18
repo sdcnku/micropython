7b2f0834b2b96ce0691bb34b6dcd28c1900fc535
iabdalkader <i.abdalkader@gmail.com>
2022-02-18 04:25:00 +0200

    OPENMVPT: Fix RTC LSE.

--- ports/stm32/boards/OPENMVPT/mpconfigboard.h
+++ ports/stm32/boards/OPENMVPT/mpconfigboard.h
@@ -26,6 +26,7 @@
 
 // Use external 32kHz crystal for the RTC.
 #define MICROPY_HW_RTC_USE_LSE      (1)
+#define MICROPY_HW_RTC_USE_BYPASS   (1)
 #define MICROPY_HW_RTC_USE_US       (1)
 #define MICROPY_HW_RTC_USE_CALOUT   (0)
 
