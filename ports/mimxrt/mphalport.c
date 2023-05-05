89c79bb642e0707337481e61bc44b91efd968db4
iabdalkader <i.abdalkader@gmail.com>
2023-05-05 11:29:39 +0200

    mimxrt/mphalport.c: Link USB debug.

--- ports/mimxrt/mphalport.c
+++ ports/mimxrt/mphalport.c
@@ -34,6 +34,7 @@
 #include "ticks.h"
 #include "tusb.h"
 #include "fsl_snvs_lp.h"
+#include "tinyusb_debug.h"
 
 #ifndef MICROPY_HW_STDIN_BUFFER_LEN
 #define MICROPY_HW_STDIN_BUFFER_LEN 512
@@ -47,6 +48,8 @@ ringbuf_t stdin_ringbuf = {stdin_ringbuf_array, sizeof(stdin_ringbuf_array), 0, 
 uint8_t cdc_itf_pending; // keep track of cdc interfaces which need attention to poll
 
 void poll_cdc_interfaces(void) {
+    tud_task();
+
     // any CDC interfaces left to poll?
     if (cdc_itf_pending && ringbuf_free(&stdin_ringbuf)) {
         for (uint8_t itf = 0; itf < 8; ++itf) {
@@ -60,12 +63,12 @@ void poll_cdc_interfaces(void) {
     }
 }
 
-
 void tud_cdc_rx_cb(uint8_t itf) {
     // consume pending USB data immediately to free usb buffer and keep the endpoint from stalling.
     // in case the ringbuffer is full, mark the CDC interface that need attention later on for polling
     cdc_itf_pending &= ~(1 << itf);
-    for (uint32_t bytes_avail = tud_cdc_n_available(itf); bytes_avail > 0; --bytes_avail) {
+    for (uint32_t bytes_avail = tud_cdc_n_available(itf);
+            !tinyusb_debug_enabled() && bytes_avail > 0; --bytes_avail) {
         if (ringbuf_free(&stdin_ringbuf)) {
             int data_char = tud_cdc_read_char();
             if (data_char == mp_interrupt_char) {
@@ -110,14 +113,16 @@ int mp_hal_stdin_rx_chr(void) {
 }
 
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
-    if (tud_cdc_connected()) {
+    tinyusb_debug_tx_strn(str, len);
+
+    if (tud_cdc_connected() && !tinyusb_debug_enabled()) {
         for (size_t i = 0; i < len;) {
             uint32_t n = len - i;
             if (n > CFG_TUD_CDC_EP_BUFSIZE) {
                 n = CFG_TUD_CDC_EP_BUFSIZE;
             }
             while (n > tud_cdc_write_available()) {
-                __WFE();
+                MICROPY_EVENT_POLL_HOOK
             }
             uint32_t n2 = tud_cdc_write(str + i, n);
             tud_cdc_write_flush();
