7811e399e402ed9982d570a24914167590d79545
iabdalkader <i.abdalkader@gmail.com>
2024-01-25 12:02:30 +0200

    rp2/boards/ARDUINO_NANO_RP2040_CONNECT: Increase flash storage space.
    Fixes issue #13512.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
+++ ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
@@ -3,7 +3,7 @@
 
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME           "Arduino Nano RP2040 Connect"
-#define MICROPY_HW_FLASH_STORAGE_BYTES  (8 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES  (14 * 1024 * 1024)
 
 // Bootloader config.
 extern void pico_reset_to_bootloader(size_t n_args, const void *args_in);
