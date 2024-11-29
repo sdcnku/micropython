2aff7317226ba6865888368fa3418916d03bd890
iabdalkader <i.abdalkader@gmail.com>
2024-11-29 13:56:22 +0100

    extmod/modmachine: Add machine.CAN.

--- extmod/modmachine.h
+++ extmod/modmachine.h
@@ -202,6 +202,7 @@ extern const machine_mem_obj_t machine_mem32_obj;
 // is provided by a port.
 extern const mp_obj_type_t machine_adc_type;
 extern const mp_obj_type_t machine_adc_block_type;
+extern const mp_obj_type_t machine_can_type;
 extern const mp_obj_type_t machine_i2c_type;
 extern const mp_obj_type_t machine_i2s_type;
 extern const mp_obj_type_t machine_mem_type;
