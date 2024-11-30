b204462e6f55f1f57ada907aa77484fb0b14a649
iabdalkader <i.abdalkader@gmail.com>
2024-11-30 09:10:54 +0100

    ports/stm32/timer: Switch the default servo timer to TIM4.
    All older boards use TIM4 for the PWM.

--- ports/stm32/stm32_it.c
+++ ports/stm32/stm32_it.c
@@ -639,6 +639,7 @@ void TIM3_IRQHandler(void) {
 void TIM4_IRQHandler(void) {
     IRQ_ENTER(TIM4_IRQn);
     timer_irq_handler(4);
+    HAL_TIM_IRQHandler(&TIM4_Handle);
     IRQ_EXIT(TIM4_IRQn);
 }
 #endif
@@ -646,7 +647,6 @@ void TIM4_IRQHandler(void) {
 void TIM5_IRQHandler(void) {
     IRQ_ENTER(TIM5_IRQn);
     timer_irq_handler(5);
-    HAL_TIM_IRQHandler(&TIM5_Handle);
     IRQ_EXIT(TIM5_IRQn);
 }
 
